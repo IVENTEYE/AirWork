13ca1472d079cdd72ce14f4b174710d64337198e
IVENTEYE <52000764+IVENTEYE@users.noreply.github.com>
2020-06-24 17:37:26 +0600

    Delete script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector('.right-items__image-info-btn'),
-      cookies = document.querySelector('.right-items__image-info');
-
-button.addEventListener('click', (e) => {
-    e.preventDefault();
-    cookies.classList.add('remove');
-});
-
-new WOW().init();
